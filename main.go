59fae90a87081b16dfa44082cf407f72bdccb0fc
Chris Helming <chris.helming@gmail.com>
2024-10-21 13:08:31 -0400

    adding ability to rename calendar

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
     defer resp.Body.Close()
     bodyBytes, err := ioutil.ReadAll(resp.Body)
     bodyString := string(bodyBytes)
-    bodyString = strings.Replace(bodyString, "\r\n", "%%", -1)
+    bodyString = strings.Replace(bodyString, "\r\n", "%%", -1) // join to make regex easier
     re := regexp.MustCompile(`(BEGIN:VEVENT.*?END:VEVENT%%)`)
     matches := re.FindAllStringSubmatch(bodyString, -1)
     for _, match := range matches {
@@ -27,7 +27,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
                bodyString = strings.Replace(bodyString, match[0], "", -1)
 	    } 
     }
-    fmt.Fprint(w, strings.Replace(bodyString, "%%", "\r\n", -1))
+    bodyString = strings.Replace(bodyString, "%%", "\r\n", -1) // split back to separate lines
+
+    newCalName := fmt.Sprintf("%s%s", "X-WR-CALNAME:", os.Getenv("DISPLAY_NAME"))
+    bodyString = strings.Replace(bodyString, "X-WR-CALNAME:Calendar", newCalName, -1) // rename the calendar
+
+    fmt.Fprint(w, bodyString)
 }
 
 func main() {
